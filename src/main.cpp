d0c74c4626ad78711b3177b687a43edc5cff54c7
rasm947f <rasm947f@edu.sde.dk>
2022-10-26 16:49:37 +0200

    Working ENV sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <M5Stack.h>
+#include <M5_ENV.h>
+#include <Adafruit_BMP280.h>
 
 SHT3X sht30;
 QMP6988 qmp6988;
@@ -20,7 +22,7 @@ void setup()
   M5.lcd.setTextSize(2); // Set the text size to 2.
   Wire.begin();          // Wire init, adding the I2C bus.
   qmp6988.init();
-  M5.lcd.println(F("ENVIII Unit(SHT30 and QMP6988) test"));
+  //M5.lcd.println(F("ENVIII Unit(SHT30 and QMP6988) test"));
 }
 
 void loop()
@@ -36,9 +38,7 @@ void loop()
   {
     tmp = 0, hum = 0;
   }
-  M5.lcd.fillRect(0, 20, 100, 60,
-                  BLACK); // Fill the screen with black (to clear the
-                          // screen).
+  M5.lcd.fillRect(0, 20, 100, 60, BLACK); // Fill the screen with black (to clear the screen).
   M5.lcd.setCursor(0, 20);
   M5.Lcd.printf("Temp: %2.1f  \r\nHumi: %2.0f%%  \r\nPressure:%2.0fPa\r\n",
                 tmp, hum, pressure);
