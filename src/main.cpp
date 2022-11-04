ee011484648e77c3726b918d27750a3b6f6dd62f
rasm947f <54017710+rasm947f@users.noreply.github.com>
2022-11-04 12:10:30 +0100

    Danish text, Changes to data for API and a fixes

--- src/main.cpp
+++ src/main.cpp
@@ -15,21 +15,23 @@ PubSubClient client(espClient);
 // Configure the name and password of the connected wifi and your MQTT Serve host.
 const char *ssid = "CoronaMecca";
 const char *password = "ckebab1234";
-const char *mqtt_server = "192.168.1.114";
+const char *mqtt_server = "CoronaMeccaServer";
 const char *mqtt_username = "client";
 const char *mqtt_pass = "test";
 const char *mqtt_topic = "test";
 
 // Variables
 unsigned long lastMsg = 0;
-#define MSG_BUFFER_SIZE (50)
+#define MSG_BUFFER_SIZE (60)
 #define ANN_BUFFER_SIZE (17)
+#define ID_BUFFER_SIZE (4)
 char announcement[ANN_BUFFER_SIZE];
 char msg[MSG_BUFFER_SIZE];
 char display_temp[MSG_BUFFER_SIZE];
 char display_humi[MSG_BUFFER_SIZE];
+char client_ID[ID_BUFFER_SIZE];
 bool running;
-int zone_id = 1;
+int zone_ID = 1;
 
 // FUNCTIONS
 // Wifi
@@ -50,7 +52,10 @@ void setup()
     M5.Power.begin();
     M5.Lcd.setTextDatum(MC_DATUM); // Set text alignment to center
     M5.lcd.setTextSize(3);
-    M5.Lcd.printf("Zone %d", zone_id);
+
+    snprintf(client_ID, MSG_BUFFER_SIZE, "%d", zone_ID); // Format to the specified string and store it in MSG.
+
+    M5.Lcd.printf("Zone %d", zone_ID);
 
     // Wifi and MQTT
     M5.lcd.setTextSize(1);   // Change text size for messages
@@ -66,7 +71,7 @@ void setup()
 
     // Default message
     M5.lcd.setTextSize(2); // Change text size
-    M5.Lcd.println("Press 'Left' to send data");
+    M5.Lcd.println("Tryk 'A' for at starte");
 }
 
 void loop()
@@ -74,6 +79,8 @@ void loop()
     // M5.Lcd.clear(); // Clear the screen.
     if (!client.connected())
     {
+        M5.Lcd.setCursor(0, 56); // Set cursor to top of screen
+        M5.lcd.fillRect(0, 56, 320, 184, BLACK); // Clear the screen
         connect();
     }
 
@@ -88,23 +95,23 @@ void loop()
     {
         if (!running && lastMsg == 0)
         {
-            M5.lcd.fillRect(0, 30, 320, 190, BLACK);        // Fill the screen with black (to clear the screen).
-            M5.Lcd.drawString("Temperature:", 140, 110, 2); // Display temperature on the controller
-            M5.Lcd.drawString("Humidity:", 150, 140, 2);    // Display humidity on the controller
-            
+            M5.lcd.fillRect(0, 30, 320, 190, BLACK);          // Fill the screen with black (to clear the screen).
+            M5.Lcd.drawString("Temperatur:", 140, 110, 2);    // Display temperature on the controller
+            M5.Lcd.drawString("Luftfugtighed:", 130, 140, 2); // Display humidity on the controller
+
             getEnvData(); // Get environmentdata and display it
         }
-        
+
         if (!running)
         {
             running = true;
             M5.lcd.fillRect(170, 0, 140, 20, BLACK); // Clear running/not running state
-            M5.Lcd.drawString("Running", 263, 0, 1); // Display state on the controller
+            M5.Lcd.drawString("Sender", 263, 0, 1);  // Display state on the controller
         }
         else
         {
             running = false;
-            M5.Lcd.drawString("Not Running", 240, 0, 1); // Display state on the controller
+            M5.Lcd.drawString("Sender ikke", 240, 0, 1); // Display state on the controller
         }
     }
 
@@ -123,17 +130,17 @@ void loop()
     if (M5.BtnB.wasPressed())
     {
         M5.lcd.fillRect(220, 100, 80, 30, BLACK); // Fill the screen with black (to clear the temperature).
-        M5.lcd.fillRect(210, 130, 60, 30, BLACK); // Fill the screen with black (to clear the humidity).
+        M5.lcd.fillRect(220, 130, 60, 30, BLACK); // Fill the screen with black (to clear the humidity).
 
         M5.Lcd.drawString("-18.6", 260, 110, 2); // Display temperature on the controller
-        M5.Lcd.drawString("49%", 240, 140, 2);   // Display humidity on the controller
+        M5.Lcd.drawString("49%", 250, 140, 2);   // Display humidity on the controller
     }
 }
 
 void setupWifi()
 {
     delay(10);
-    M5.Lcd.printf("Connecting to %s", ssid);
+    M5.Lcd.printf("Forbinder til WIFI [%s]", ssid);
     WiFi.mode(WIFI_STA);        // Set the mode to WiFi station mode.
     WiFi.begin(ssid, password); // Start Wifi connection.
 
@@ -142,32 +149,41 @@ void setupWifi()
         delay(500);
         M5.Lcd.print(".");
     }
-    M5.Lcd.printf("\nSuccess\n");
+    M5.Lcd.printf("\nSucces\n");
 }
 
 void connect()
 {
+    int value = 0;
+
     while (!client.connected())
     {
-        M5.Lcd.print("Attempting MQTT connection...");
-        // Create a random client ID.
-        String clientId = "M5Stack-";
-        clientId += String(random(0xffff), HEX);
+        M5.lcd.setTextSize(1); // Set font-size to 1
+        M5.Lcd.println("Forsoeger MQTT forbindelse...");
         // Attempt to connect.
-        if (client.connect(clientId.c_str(), "client", "pass"))
+        if (client.connect(client_ID, "client", "pass"))
         {
-            M5.Lcd.printf("\nSuccess\n");
+            M5.Lcd.printf("Succes\n");
             // Once connected, publish an announcement to the topic.
-            snprintf(announcement, ANN_BUFFER_SIZE, "Zone %d connected", zone_id); // Format to the specified string and store it in a variable.
+            snprintf(announcement, ANN_BUFFER_SIZE, "Zone %d connected", zone_ID); // Format to the specified string and store it in a variable.
 
             client.publish(mqtt_topic, announcement);
+            M5.lcd.setTextSize(2); // Change font-size back to 2
         }
         else
         {
-            M5.Lcd.print("failed, rc=");
-            M5.Lcd.print(client.state());
-            M5.Lcd.println("try again in 5 seconds");
-            delay(5000);
+            M5.Lcd.print("Fejl, rc=");
+            M5.Lcd.println(client.state());
+            M5.Lcd.println("Proever igen om 5 sekunder");
+
+            value++;
+
+            delay(10000);
+            if (value % 7 == 0)
+            {
+                M5.Lcd.setCursor(0, 56);
+                M5.lcd.fillRect(0, 56, 320, 184, BLACK); // Fill the screen with black.
+            }
         }
     }
 }
@@ -185,14 +201,14 @@ void getEnvData()
         temp = 0, humi = 0;
     }
 
-    snprintf(msg, MSG_BUFFER_SIZE, "{Temperature: %2.1f, Humidity: %2.0f%%}", temp, humi); // Format to the specified string and store it in MSG.
-    snprintf(display_temp, MSG_BUFFER_SIZE, "%2.1f", temp);                                // Format string for displaying temperature on the controller
-    snprintf(display_humi, MSG_BUFFER_SIZE, "%2.0f%%", humi);                              // Format string for displaying humidity on the controller
+    snprintf(msg, MSG_BUFFER_SIZE, "{\"zone_ID\": %d, \"temperature\": \"%2.1f\", \"humidity\": \"%2.0f\"}", zone_ID, temp, humi); // Format to the specified string and store it in MSG.
+    snprintf(display_temp, MSG_BUFFER_SIZE, "%2.1f", temp);                                                                        // Format string for displaying temperature on the controller
+    snprintf(display_humi, MSG_BUFFER_SIZE, "%2.0f%%", humi);                                                                      // Format string for displaying humidity on the controller
 
     M5.lcd.fillRect(220, 100, 80, 30, BLACK); // Fill the screen with black (to clear the temperature).
-    M5.lcd.fillRect(210, 130, 60, 30, BLACK); // Fill the screen with black (to clear the humidity).
+    M5.lcd.fillRect(220, 130, 60, 30, BLACK); // Fill the screen with black (to clear the humidity).
 
     // M5.lcd.setCursor(0, 100);
     M5.Lcd.drawString(display_temp, 265, 110, 2); // Display temperature on the controller
-    M5.Lcd.drawString(display_humi, 240, 140, 2); // Display humidity on the controller
+    M5.Lcd.drawString(display_humi, 250, 140, 2); // Display humidity on the controller
 }
